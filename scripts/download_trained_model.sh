6f6709145ca2fa60d14663317f87d8cdcb5d8223
Hang Zhao <zhaohang0124@gmail.com>
2019-03-05 23:32:28 -0500

    minor

--- scripts/download_trained_model.sh
+++ scripts/download_trained_model.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 MODEL_ID=MUSIC-2mix-LogFreq-resnet18dilated-unet7-linear-frames3stride24-maxpool-binary-weightedLoss-channels32-epoch100-step40_80
 MODEL_PATH=./ckpt/$MODEL_ID
@@ -7,7 +8,6 @@ LIST_VAL=$LINK_RELEASE/val.csv
 WEIGHTS_FRAME=$LINK_RELEASE/${MODEL_ID}/frame_best.pth
 WEIGHTS_SOUND=$LINK_RELEASE/${MODEL_ID}/sound_best.pth
 WEIGHTS_SYNTHESIZER=$LINK_RELEASE/${MODEL_ID}/synthesizer_best.pth
-
 
 # Download val list model weights
 wget -P ./data $LIST_VAL
